447f7fd151ac04eabe700dca55e28dc9daadc340
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2024-02-17 20:54:43 +0100

    rewrite Startfiles.java & json.java

--- src/main/java/com/bedrockcloud/bedrockcloud/utils/files/Startfiles.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/utils/files/Startfiles.java
@@ -1,167 +1,112 @@
 package com.bedrockcloud.bedrockcloud.utils.files;
 
 import com.bedrockcloud.bedrockcloud.BedrockCloud;
+import com.bedrockcloud.bedrockcloud.api.GroupAPI;
 import com.bedrockcloud.bedrockcloud.api.PasswordAPI;
 import com.bedrockcloud.bedrockcloud.utils.config.Config;
 import com.bedrockcloud.bedrockcloud.utils.console.Loggable;
-import com.bedrockcloud.bedrockcloud.api.GroupAPI;
 import com.bedrockcloud.bedrockcloud.SoftwareManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class Startfiles implements Loggable
-{
-    private final ArrayList<String> directorys;
+public class Startfiles implements Loggable {
+    private final ArrayList<String> directories;
     private final int cloudPort;
 
     public Startfiles(int cloudPort) {
         this.cloudPort = cloudPort;
-
-        this.directorys = new ArrayList<>();
-        try {
-            this.delete(new File("./temp"));
-            this.directorys.add("./templates");
-            this.directorys.add("./temp");
-            this.directorys.add("./local");
-            this.directorys.add("./archive");
-            this.directorys.add(this.directorys.get(2) + "/plugins");
-            this.directorys.add(this.directorys.get(2) + "/plugins/cloud");
-            this.directorys.add(this.directorys.get(2) + "/plugins/pocketmine");
-            this.directorys.add(this.directorys.get(2) + "/plugins/waterdogpe");
-            this.directorys.add(this.directorys.get(2) + "/versions");
-            this.directorys.add(this.directorys.get(2) + "/versions/pocketmine");
-            this.directorys.add(this.directorys.get(2) + "/versions/waterdogpe");
-            this.directorys.add(this.directorys.get(2) + "/notify");
-            this.directorys.add(this.directorys.get(3) + "/crashdumps");
-            this.directorys.add(this.directorys.get(3) + "/server-pids");
-        } catch (NullPointerException e) {
-            this.getLogger().exception(e);
-        }
-        this.checkFolder();
+        this.directories = new ArrayList<>();
+        initializeDirectories();
+        checkFolders();
     }
 
-    private void delete(final File file) {
-        if (file.isDirectory()) {
-            final String[] fileList = file.list();
-            if (fileList.length == 0) {
-                file.delete();
-            } else {
-                for (final String fileName : fileList) {
-                    final String fullPath = file.getPath() + "/" + fileName;
-                    final File fileOrFolder = new File(fullPath);
-                    this.delete(fileOrFolder);
-                }
-                this.delete(file);
-            }
-        } else {
-            file.delete();
-        }
+    private void initializeDirectories() {
+        this.directories.add("./templates");
+        this.directories.add("./temp");
+        this.directories.add("./local");
+        this.directories.add("./archive");
+        this.directories.add(this.directories.get(2) + "/plugins");
+        this.directories.add(this.directories.get(2) + "/plugins/cloud");
+        this.directories.add(this.directories.get(2) + "/plugins/pocketmine");
+        this.directories.add(this.directories.get(2) + "/plugins/waterdogpe");
+        this.directories.add(this.directories.get(2) + "/versions");
+        this.directories.add(this.directories.get(2) + "/versions/pocketmine");
+        this.directories.add(this.directories.get(2) + "/versions/waterdogpe");
+        this.directories.add(this.directories.get(2) + "/notify");
+        this.directories.add(this.directories.get(3) + "/crashdumps");
+        this.directories.add(this.directories.get(3) + "/server-pids");
     }
 
-    private void checkFolder() {
+    private void checkFolders() {
         try {
-            for (final String direc : this.directorys) {
-                final File theDir = new File(direc);
-                if (!theDir.exists()) {
-                    this.getLogger().debug("Creating Folder in the Directory " + direc + "!");
-                    theDir.mkdirs();
+            for (String directory : this.directories) {
+                File dir = new File(directory);
+                if (!dir.exists()) {
+                    getLogger().debug("Creating directory " + directory + "!");
+                    dir.mkdirs();
                 }
             }
-            final File templatesc = new File("./templates/config.json");
-            if (!templatesc.exists()) {
-                templatesc.createNewFile();
-            }
-
-            final File file = new File("./local/config.yml");
-
-            if (!file.exists()) {
-                Config config = new Config(file, Config.YAML);
-                config.set("port", (double)this.cloudPort);
-                config.set("debug-mode", false);
-                config.set("motd", "Default BedrockCloud Service");
-                config.set("auto-update-on-start", false);
-                config.set("wdpe-login-extras", false);
-                config.set("enable-cloudlog-file", false);
-                config.set("use-proxy", true);
-                config.set("auto-restart-cloud", false);
-                config.set("rest-password", PasswordAPI.generateRandomPassword(8));
-                config.set("rest-port", 8080.0);
-                config.set("rest-username", "cloud");
-                config.set("rest-enabled", true);
-                config.set("service-separator", "-");
-                config.save(file);
-            }
-
-            final File maintenance = new File("./local/maintenance.txt");
-            if (!maintenance.exists()){
-                Config config = new Config(file, Config.ENUM);
-                config.save();
-            }
-
-            final File pocketmineFile = new File("./local/versions/pocketmine/PocketMine-MP.phar");
-            final File waterdogFile = new File("./local/versions/waterdogpe/WaterdogPE.jar");
-
-            if (!pocketmineFile.exists()) {
-                SoftwareManager.downloadAsync(SoftwareManager.POCKETMINE_URL, "./local/versions/pocketmine/PocketMine-MP.phar").whenComplete((success, error) -> {
-                    if (success) {
-                        BedrockCloud.getLogger().info("WaterdogPE downloaded!");
-                    } else {
-                        BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
-                    }
-                });
-            }
-
-            if (!waterdogFile.exists()) {
-                SoftwareManager.downloadAsync(SoftwareManager.WATERDOGPE_URL, "./local/versions/waterdogpe/WaterdogPE.jar").whenComplete((success, error) -> {
-                    if (success) {
-                        BedrockCloud.getLogger().info("WaterdogPE downloaded!");
-                    } else {
-                        BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
-                    }
-                });
-            }
-
-            final File cloudbridgepmFile = new File("./local/plugins/pocketmine/CloudBridge-PM.phar");
-            final File devtools = new File("./local/plugins/pocketmine/DevTools.phar");
-            final File cloudbridgewdFile = new File("./local/plugins/waterdogpe/CloudBridge-WD.jar");
-
-            if (!cloudbridgepmFile.exists()) {
-                SoftwareManager.downloadAsync(SoftwareManager.CLOUDBRIDGEPM_URL, "./local/plugins/pocketmine/CloudBridge-PM.phar").whenComplete((success, error) -> {
-                    if (success) {
-                        BedrockCloud.getLogger().info("CloudBridge-PM downloaded!");
-                    } else {
-                        BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
-                    }
-                });
-            }
-
-            if (!devtools.exists()) {
-                SoftwareManager.downloadAsync(SoftwareManager.DEVTOOLS_URL, "./local/plugins/pocketmine/DevTools.phar").whenComplete((success, error) -> {
-                    if (success) {
-                        BedrockCloud.getLogger().info("DevTools downloaded!");
-                    } else {
-                        BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
-                    }
-                });
-            }
-
-            if (!cloudbridgewdFile.exists()) {
-                SoftwareManager.downloadAsync(SoftwareManager.CLOUDBRIDGEWD_URL, "./local/plugins/waterdogpe/CloudBridge-WD.jar").whenComplete((success, error) -> {
-                    if (success) {
-                        BedrockCloud.getLogger().info("CloudBridge-WD downloaded!");
-                    } else {
-                        BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
-                    }
-                });
-            }
-            GroupAPI.createGroup("Proxy-Master", SoftwareManager.SOFTWARE_PROXY);
-            GroupAPI.createGroup("Lobby", SoftwareManager.SOFTWARE_SERVER);
-
-            BedrockCloud.getLogger().info("§aStarting cloud...");
+            createConfigFiles();
+            downloadMissingFiles();
+            createDefaultGroups();
+            getLogger().info("§aStarting cloud...");
             Thread.sleep(3000);
         } catch (Exception e) {
-            this.getLogger().exception(e);
+            getLogger().exception(e);
         }
+    }
+
+    private void createConfigFiles() throws IOException {
+        File localConfigFile = new File("./local/config.yml");
+        if (!localConfigFile.exists()) {
+            Config config = new Config(localConfigFile, Config.YAML);
+            config.set("port", (double) this.cloudPort);
+            config.set("debug-mode", false);
+            config.set("motd", "Default BedrockCloud Service");
+            config.set("auto-update-on-start", false);
+            config.set("wdpe-login-extras", false);
+            config.set("enable-cloudlog-file", false);
+            config.set("use-proxy", true);
+            config.set("auto-restart-cloud", false);
+            config.set("rest-password", PasswordAPI.generateRandomPassword(8));
+            config.set("rest-port", 8080.0);
+            config.set("rest-username", "cloud");
+            config.set("rest-enabled", true);
+            config.set("service-separator", "-");
+            config.save();
+        }
+
+        File maintenanceFile = new File("./local/maintenance.txt");
+        if (!maintenanceFile.exists()) {
+            maintenanceFile.createNewFile();
+        }
+    }
+
+    private void downloadMissingFiles() {
+        downloadFile(SoftwareManager.POCKETMINE_URL, "./local/versions/pocketmine/PocketMine-MP.phar");
+        downloadFile(SoftwareManager.WATERDOGPE_URL, "./local/versions/waterdogpe/WaterdogPE.jar");
+        downloadFile(SoftwareManager.CLOUDBRIDGEPM_URL, "./local/plugins/pocketmine/CloudBridge-PM.phar");
+        downloadFile(SoftwareManager.DEVTOOLS_URL, "./local/plugins/pocketmine/DevTools.phar");
+        downloadFile(SoftwareManager.CLOUDBRIDGEWD_URL, "./local/plugins/waterdogpe/CloudBridge-WD.jar");
+    }
+
+    private void downloadFile(String url, String destination) {
+        File file = new File(destination);
+        if (!file.exists()) {
+            SoftwareManager.downloadAsync(url, destination).whenComplete((success, error) -> {
+                if (success) {
+                    BedrockCloud.getLogger().info(file.getName() + " downloaded!");
+                } else {
+                    BedrockCloud.getLogger().error("Download failed: " + error.getMessage());
+                }
+            });
+        }
+    }
+
+    private void createDefaultGroups() {
+        GroupAPI.createGroup("Proxy-Master", SoftwareManager.SOFTWARE_PROXY);
+        GroupAPI.createGroup("Lobby", SoftwareManager.SOFTWARE_SERVER);
     }
 }
--- src/main/java/com/bedrockcloud/bedrockcloud/utils/files/json/json.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/utils/files/json/json.java
@@ -1,110 +1,70 @@
 package com.bedrockcloud.bedrockcloud.utils.files.json;
 
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
-import org.json.simple.JSONObject;
 import java.util.HashMap;
 import org.json.simple.JSONArray;
-import java.io.Reader;
-import java.io.FileReader;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-public class json
-{
-    public static final int minRunningServer = 0;
-    public static final int maxRunningServer = 1;
-    public static final int maxPlayer = 2;
-    public static final int maintenance = 3;
-    public static final int beta = 4;
-    public static final int proxy = 5;
-    public static final int type = 6;
-    public static final int isLobby = 7;
-    public static final int isStatic = 8;
+public class json {
+
+    public static final int MIN_RUNNING_SERVER = 0;
+    public static final int MAX_RUNNING_SERVER = 1;
+    public static final int MAX_PLAYER = 2;
+    public static final int MAINTENANCE = 3;
+    public static final int BETA = 4;
+    public static final int PROXY = 5;
+    public static final int TYPE = 6;
+    public static final int IS_LOBBY = 7;
+    public static final int IS_STATIC = 8;
     public static final int ALL = 9;
-    
+
     public static Object get(final String name, final int type) throws IOException {
         final JSONParser jsonParser = new JSONParser();
-        try {
-            final FileReader reader = new FileReader("./templates/config.json");
-            try {
-                final Object obj = jsonParser.parse(reader);
-                final JSONArray template = (JSONArray)obj;
-                final Object[] returnval = { null };
-                final HashMap<String, Object> stats = new HashMap<String, Object>();
-                for (final Object tmp : template) {
-                    final JSONObject tempobj = (JSONObject)tmp;
-                    final JSONObject directtemp = (JSONObject) tempobj.get(name);
-                    if (directtemp != null) {
-                        if (type != 9) {
-                            switch (type) {
-                                case json.minRunningServer -> {
-                                    returnval[0] = directtemp.get("minRunningServer");
-                                }
-                                case json.maxRunningServer -> {
-                                    returnval[0] = directtemp.get("maxRunningServer");
-                                }
-                                case json.maxPlayer -> {
-                                    returnval[0] = directtemp.get("maxPlayer");
-                                }
-                                case json.maintenance -> {
-                                    returnval[0] = directtemp.get("maintenance");
-                                }
-                                case json.beta -> {
-                                    returnval[0] = directtemp.get("beta");
-                                }
-                                case json.proxy -> {
-                                    returnval[0] = directtemp.get("proxy");
-                                }
-                                case json.type -> {
-                                    returnval[0] = directtemp.get("type");
-                                }
-                                case json.isLobby -> {
-                                    returnval[0] = directtemp.get("isLobby");
-                                }
-                                case json.isStatic -> {
-                                    returnval[0] = directtemp.get("isStatic");
-                                }
-                                default -> {}
-                            }
-                        } else {
-                            stats.put("minRunningServer", directtemp.get("minRunningServer"));
-                            stats.put("maxRunningServer", directtemp.get("maxRunningServer"));
-                            stats.put("maxPlayer", directtemp.get("maxPlayer"));
-                            stats.put("maintenance", directtemp.get("maintenance"));
-                            stats.put("beta", directtemp.get("beta"));
-                            stats.put("canBePrivate", directtemp.get("canBePrivate"));
-                            stats.put("proxy", directtemp.get("proxy"));
-                            stats.put("type", directtemp.get("type"));
-                            stats.put("isLobby", directtemp.get("isLobby"));
-                            stats.put("isStatic", directtemp.get("isStatic"));
+        try (FileReader reader = new FileReader("./templates/config.json")) {
+            final Object obj = jsonParser.parse(reader);
+            final JSONArray template = (JSONArray) obj;
+            final Object[] returnval = {null};
+            final HashMap<String, Object> stats = new HashMap<>();
+            for (final Object tmp : template) {
+                final JSONObject tempobj = (JSONObject) tmp;
+                final JSONObject directtemp = (JSONObject) tempobj.get(name);
+                if (directtemp != null) {
+                    if (type != ALL) {
+                        switch (type) {
+                            case MIN_RUNNING_SERVER -> returnval[0] = directtemp.get("minRunningServer");
+                            case MAX_RUNNING_SERVER -> returnval[0] = directtemp.get("maxRunningServer");
+                            case MAX_PLAYER -> returnval[0] = directtemp.get("maxPlayer");
+                            case MAINTENANCE -> returnval[0] = directtemp.get("maintenance");
+                            case BETA -> returnval[0] = directtemp.get("beta");
+                            case PROXY -> returnval[0] = directtemp.get("proxy");
+                            case TYPE -> returnval[0] = directtemp.get("type");
+                            case IS_LOBBY -> returnval[0] = directtemp.get("isLobby");
+                            case IS_STATIC -> returnval[0] = directtemp.get("isStatic");
+                            default -> {}
                         }
+                    } else {
+                        stats.put("minRunningServer", directtemp.get("minRunningServer"));
+                        stats.put("maxRunningServer", directtemp.get("maxRunningServer"));
+                        stats.put("maxPlayer", directtemp.get("maxPlayer"));
+                        stats.put("maintenance", directtemp.get("maintenance"));
+                        stats.put("beta", directtemp.get("beta"));
+                        stats.put("proxy", directtemp.get("proxy"));
+                        stats.put("type", directtemp.get("type"));
+                        stats.put("isLobby", directtemp.get("isLobby"));
+                        stats.put("isStatic", directtemp.get("isStatic"));
                     }
                 }
-                if (stats.isEmpty()) {
-                    final Object object = returnval[0];
-                    reader.close();
-                    return object;
-                }
-                final HashMap<String, Object> hashMap1 = stats;
-                reader.close();
-                return hashMap1;
             }
-            catch (Throwable throwable) {
-                try {
-                    reader.close();
-                }
-                catch (Throwable throwable2) {
-                    throwable.addSuppressed(throwable2);
-                }
-                throw throwable;
+            if (stats.isEmpty()) {
+                return returnval[0];
             }
-        }
-        catch (FileNotFoundException | ParseException ex2) {
-            final Exception ex = new Exception();
-            final Exception e = ex;
-            e.printStackTrace();
+            return stats;
+        } catch (FileNotFoundException | ParseException ex) {
+            ex.printStackTrace();
             return false;
         }
     }
